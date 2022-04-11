a6a6b506d25d13ad666fb708b33e256aaea41e44
Zach Keller <zkellerz68@gmail.com>
2022-04-11 14:08:36 -0400

    login updated

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -12,27 +12,36 @@
         role="alert"
         v-if="this.$route.query.registration"
       >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
+      <section>
+        <label for="username" class="sr-only">Username</label>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Username"
+          v-model="user.username"
+          required
+          autofocus
+        />
+      </section>
+
+      <section>
+        <label for="password" class="sr-only">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
+      </section>
+
+      <section>
       <button type="submit">Sign in</button>
+      <br>
+      <router-link :to="{ name: 'register' }">Need an account?</router-link>
+      </section>
     </form>
   </div>
 </template>
