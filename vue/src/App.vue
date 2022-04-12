dbb88a83bf5e78df9e8b48fc605d16d37b04b6a9
Ahmet Yildirim <yldrmao@gmail.com>
2022-04-12 10:19:06 -0400

    brewery page is in progress

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,15 +1,52 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <div id="home">
+        <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;&nbsp;
+      </div>
+      <div id="login">
+         <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+         <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
+      </div>
+     
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@900&display=swap');
   html{
     text-align: center;
+    background-color: #DEB887;
+  }
+  #nav{
+    margin: 0;
+    position: sticky;
+    display: grid;
+    top: 0;
+    /* background-color: white; */
+    grid-template: " home  login";
+    font-family: 'Playfair Display', serif;
+    font-weight: 1000;
+  }
+  #nav a {
+    padding: 1em;
+    background-color: #ffffff80;
+    padding: 1em;
+    border-radius: 15px;
+  }
+  #home{
+    display: flex;
+    justify-content: flex-start;
+    grid-area:"home";
+    padding-left: 2em;
+  }
+
+  #login{
+    display: flex;
+    justify-content: flex-end;
+    grid-area: "login";
+    padding-right: 2em;
   }
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="home">
     <h1>The Brewery Finder 🍻</h1>
-    <p>You must be authenticated to see this</p>
-    <brewery-list/>
+    <!-- <p>You must be authenticated to see this</p> -->
+    <brewery-list />
   </div>
 </template>
 
 <script>
-import BreweryList from '../components/BreweryList.vue';
+import BreweryList from "../components/BreweryList.vue";
 export default {
   components: { BreweryList },
-  name: "home"
+  name: "home",
 };
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@900&display=swap');
+.home {
+  font-family: "Playfair Display", serif;
+}
+</style>
+
