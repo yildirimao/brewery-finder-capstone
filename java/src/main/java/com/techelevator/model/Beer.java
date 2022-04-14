6c63a855c2729ca01ddfab1484d7a703faf3a47f
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2022-04-14 10:09:14 -0400

    Created the beer model
    and the class.equals method like brewery.

---
+++ java/src/main/java/com/techelevator/model/Beer.java
@@ -0,0 +1,87 @@
+package com.techelevator.model;
+
+import java.util.Objects;
+
+public class Beer {
+
+    private int id;
+    private String name;
+    private String imgURL;
+    private String description;
+    private String abv;
+    private String type;
+
+    public Beer(){
+
+    }
+
+    public Beer(int id, String name, String imgURL, String description, String abv, String type) {
+        this.id = id;
+        this.name = name;
+        this.imgURL = imgURL;
+        this.description = description;
+        this.abv = abv;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAbv() {
+        return abv;
+    }
+
+    public void setAbv(String abv) {
+        this.abv = abv;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Beer beer = (Beer) o;
+        return id == beer.id &&
+                Objects.equals(name, beer.name) &&
+                Objects.equals(imgURL, beer.imgURL) &&
+                Objects.equals(description, beer.description) &&
+                Objects.equals(abv, beer.abv) &&
+                Objects.equals(type, beer.type);
+    }
+}
