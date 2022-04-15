5d128c569dd6387ad63a917d12453646e49db5b2
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2022-04-15 10:35:27 -0400

    minor fixes, can now add new breweries
    Refactored variables in new brewery form to match Java names, so it doesn't cause issues.
    
    set the "showform" values to false for both forms, so that they start hidden.

--- vue/src/components/BrewerForm.vue
+++ vue/src/components/BrewerForm.vue
@@ -27,7 +27,7 @@
 export default {
     data(){
         return {
-            showForm: 'false'
+            showForm: false
         }
     },
     methods: {
--- vue/src/components/BreweryForm.vue
+++ vue/src/components/BreweryForm.vue
@@ -8,21 +8,19 @@
           <label for="brewery-location">Location:</label><br>
           <input type="text" name="brewery-location" v-model="brewery.location"/><br>
           <label for="brewery-phone">Phone Number:</label><br>
-          <input type="text" name="brewery-phone" v-model="brewery.phone_number"/><br>
+          <input type="text" name="brewery-phone" v-model="brewery.phoneNumber"/><br>
           <label for="brewery-hours">Hours of Operation:</label><br>
-          <input type="text" name="brewery-hours" v-model="brewery.hours_of_operation"/><br>
+          <input type="text" name="brewery-hours" v-model="brewery.hoursOfOperation"/><br>
           <label for="brewery-address">Address:</label><br>
           <input type="text" name="brewery-address" v-model="brewery.address"/><br>
           <label for="brewery-bio">History:</label><br>
           <input type="text" name="brewery-bio" v-model="brewery.bio"/><br>
           <label for="brewery-images">Enter an https:// URL for images:</label> <br />
           <input type="url" name="brewery-img" placeholder="https://exampleimg.com"  pattern="https://.*" size="20"
-       required v-model="brewery.brewery_img" /> <br />
+       required v-model="brewery.imgUrl" /> <br />
           <p>Brewery Status:</p>
           <label for="active">Active</label>
-          <input type="checkbox" name="active" checked="Active" v-model="brewery.active"/>
-          <label for="active">Inactive</label>
-          <input type="checkbox" name="inactive" checked="Inactive" v-model="brewery.active"/><br>
+          <input type="checkbox" name="active" checked="Active" v-model="brewery.isActive"/>
           <button @click="submitBrewery">Submit</button>
       </form>
   </div>
@@ -34,17 +32,17 @@ export default {
     data(){
         return {
             brewery: {
-                brewery_id: -1,
-                brewery_name: '',
-                brewery_location: '',
-                phone_number: '',
-                hours_of_operation: '',
+                id: -1,
+                name: '',
+                location: '',
+                phoneNumber: '',
+                hoursOfOperation: '',
                 address: '',
                 bio: '',
-                brewery_img: '',
-                active: false
+                imgUrl: '',
+                isActive: false
             },
-            showForm: 'false'
+            showForm: false
         }
     },
     methods: {
