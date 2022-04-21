cc58d7c8480ad69bb32a21acf194ae6cc5a99540
Zach Keller <zkellerz68@gmail.com>
2022-04-21 10:49:52 -0400

    brewery card updated

--- vue/src/components/BreweryCard.vue
+++ vue/src/components/BreweryCard.vue
@@ -1,10 +1,9 @@
 <template>
 <!--   this is for when/if we add custom pictures per brewery-->
-  <div id="background" v-bind:style="`background-image: url(${brewery.imgUrl}); background-size: cover`">
+  <div id="background" v-bind:style="`background-image: url(${brewery.imgUrl}); background-size: cover;`">
       <div id="text">
           <h1>{{brewery.name}}</h1>
           <p>{{brewery.location}}</p>
-          
       </div>
   </div>
 </template>
@@ -17,7 +16,6 @@ export default {
 </script>
 
 <style scoped>
-/*I don't think this does anything*/
 #background{
     background-image: url("https://media.istockphoto.com/photos/draught-beer-in-glasses-picture-id1040303026");
     background-repeat: no-repeat;
@@ -32,13 +30,16 @@ export default {
 }
 
 #text{
-    color: black;
-    background-color: #ffffff80;
+    color: rgb(247, 243, 243);
+    background-color: #24222280;
     padding: 1em;
     border-radius: 15px;
+    text-transform: uppercase;
+    margin-top: auto;
 }
+
 #background:hover, #text:hover{
     cursor: pointer;
-    
 }
+
 </style>
